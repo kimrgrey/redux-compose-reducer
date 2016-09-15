fe9b3d55d42afdfa7caee7285596bedadcbc98ee
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2016-09-15 22:47:04 +0300

    Typo fix

--- createTypes.js
+++ createTypes.js
@@ -2,7 +2,7 @@ const createTypes = (namespace, actionTypes) => (
 	actionTypes.reduce((result, actionType) => {
     result[actionType] = `${namespace}/${actionType}`;
     return result;
-  }, {});
+  }, {})
 );
 
 module.exports = createTypes;
