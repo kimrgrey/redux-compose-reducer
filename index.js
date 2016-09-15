887911817c6b19629f792f33585f08560dcb2aff
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2016-09-15 20:07:32 +0300

    Fixed bug with namespacedMapping

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const composeReducer = (namespace, mapping, initialState, fallback = state => state) => {
-  const namespacedMapping = Object.keys(mapping).map(key => `${namespace}/${key}`)
+  const namespacedMapping = {};
+  
+  Object.keys(mapping).map((key) => {
+  	const newkey = `${namespace}/${key}`;
+  	namespacedMapping[newkey] = mapping[key];
+  });
+  
   return (state = initialState, action ) => {
     const handler = namespacedMapping[action.type];
     return handler ? handler(state, action) : fallback(state, action);
