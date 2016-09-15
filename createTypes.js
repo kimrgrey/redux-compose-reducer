cb3d7bc5fcdc2ee9e0c6e0de251a266736ea97a8
Sergey Tsvetkov <sergey.a.tsvetkov@gmail.com>
2016-09-15 22:58:31 +0300

    Simplifed the package

--- createTypes.js
+++
@@ -1,8 +0,0 @@
-const createTypes = (namespace, actionTypes) => (
-	actionTypes.reduce((result, actionType) => {
-    result[actionType] = `${namespace}/${actionType}`;
-    return result;
-  }, {})
-);
-
-module.exports = createTypes;
--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const composeReducer = require('./composeReducer');
-const createTypes = require('./createTypes');
+const createTypes = (namespace, actionTypes) => (
+	actionTypes.reduce((result, actionType) => {
+    result[actionType] = `${namespace}/${actionType}`;
+    return result;
+  }, {})
+);
+
+const composeReducer = (namespace, mapping, initialState, fallback = state => state) => {
+  const namespacedMapping = {};
+  
+  Object.keys(mapping).map((key) => {
+  	const newkey = `${namespace}/${key}`;
+  	namespacedMapping[newkey] = mapping[key];
+  });
+  
+  return (state = initialState, action ) => {
+    const handler = namespacedMapping[action.type];
+    return handler ? handler(state, action) : fallback(state, action);
+  }
+};
 
 module.exports = {
 	composeReducer,
