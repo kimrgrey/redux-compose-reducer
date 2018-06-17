b4abf9fe49f3906555501ec19771df6ac0b19106
AleksandrZhukov <aleksandr.a.zhukov@gmail.com>
2018-06-17 14:15:42 +0300

    changed const to var to support old browsers

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const createTypes = require('./src/createTypes');
-const composeReducer = require('./src/composeReducer');
+var createTypes = require('./src/createTypes');
+var composeReducer = require('./src/composeReducer');
 
 module.exports = {
   composeReducer: composeReducer,
